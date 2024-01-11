ff57c52bc06f5c1fbb7cb91c64a9bfc26efe59be
Mateusz Krzeszowiak <mateusz.krzeszowiak@gmail.com>
2024-01-11 19:31:15 +0100

    Fix errors

--- updateData.mjs
+++ updateData.mjs
@@ -16,11 +16,6 @@ await page.setViewport({ width: 360, height: 800 });
 // Navigate the page to a URL
 await page.goto("https://allegro.pl/kampania/one/znajdz-nas?pointId=1221448");
 
-// Wait and click on first result
-const gdprAcceptButton = 'button[data-role="accept-consent"]';
-await page.waitForSelector('button[data-role="accept-consent"]');
-await page.click(gdprAcceptButton);
-
 // Locate the full title with a unique string
 const pickupPointBox = await page.waitForSelector(
   '[data-box-name="allegro.one.map.pointDetails"]'
